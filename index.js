95ff454087b85e7b2736d0114a8b60a56d803608
CoolShivas <shivas2710cool00@gmail.com>
2025-09-13 16:23:46 +0530

    Installation of npm i mongoose. And, connecting MongoDB to ExpressJS through Mongoose.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const server = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://shivas2710cool00_db_user:PK9bmeDXdV5edZLf@cluster0.reupciq.mongodb.net/",
+    {
+      dbName: "URL_Shortener_Project",
+    }
+  )
+  .then(() => console.log("MongoDB Connected Successfully...!"))
+  .catch((err) => console.log(err));
 
 server.get("/", (request, response) => {
   response.render("index.ejs");
